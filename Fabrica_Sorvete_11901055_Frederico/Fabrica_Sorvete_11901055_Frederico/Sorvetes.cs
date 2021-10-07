8789db50aecc5fc4d4e3115d512ae0119dcf8206
Fredrreis <frederico9630@gmail.com>
2021-10-07 11:30:41 -0300

    PDF e Criacao do Projeto

---
+++ Fabrica_Sorvete_11901055_Frederico/Fabrica_Sorvete_11901055_Frederico/Sorvetes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fabrica_Sorvete_11901055_Frederico
+{
+    public partial class Sorvetes : Form
+    {
+        public Sorvetes()
+        {
+            InitializeComponent();
+        }
+    }
+}
